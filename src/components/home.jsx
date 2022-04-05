42aa6ec2d37cc28eedc7098cec57bd4dbf6281f0
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-04-05 15:57:21 +0430

    Added logout

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,25 @@
+import {useAuthState} from "react-firebase-hooks/auth";
+import {auth, firebaseLogout} from '../firebase/reactfire';
+import {useEffect} from "react";
+import {useHistory} from "react-router-dom";
+
 const Home = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    const history = useHistory();
+
+    useEffect(() => {
+        if (loading) return;
+        if (!user) history.push('/login');
+    }, [user, loading])
+
     return (
         <div>
-            خانه
+            <h1>خانه</h1>
+            <br/>
+            <span className='pointer' onClick={() => firebaseLogout()}>
+                خروج
+            </span>
         </div>
     );
 }
